301e5c01244e3210b8ef597680831af22d539d1a
osk4r99 <32760361+osk4r99@users.noreply.github.com>
2017-10-24 10:53:51 +0300

    Add files via upload

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,74 +1,33 @@
-print("Hangmantest")
-#print("Step 1")
-#print("-------------")
-#print("Step 2")
-#print("        \n        |\n        |\n        |\n        |\n        |\n-------------")
-#print("Step 3")
-#print("   _____\n        |\n        |\n        |\n        |\n        |\n-------------")
-#print("Step 4")
-#print("   _____\n   |    |\n        |\n        |\n        |\n        |\n-------------")
-#print("Step 5")
-#print("   _____\n   |    |\n   O    |\n        |\n        |\n        |\n-------------")
-#print("Step 6")
-#print("   _____\n   |    |\n   O    |\n   |    |\n        |\n        |\n-------------")
-#print("Step 7")
-#print("   _____\n   |    |\n   O    |\n  /|    |\n        |\n        |\n-------------")
-#print("Step 8")
-#print("   _____\n   |    |\n   O    |\n  /|\\   |\n        |\n        |\n-------------")
-#print("Step 9")
-#print("   _____\n   |    |\n   O    |\n  /|\\   |\n  /     |\n        |\n-------------")
-#print("Step 10")
-#print("   _____\n   |    |\n   O    |\n  /|\   |\n  / \\   |\n        |\n-------------")
-success=0
-hangman=0
-lista=["-------------",
-       "        \n        |\n        |\n        |\n        |\n        |\n-------------",
-       "   _____\n        |\n        |\n        |\n        |\n        |\n-------------",
-       "   _____\n   |    |\n        |\n        |\n        |\n        |\n-------------",
-       "   _____\n   |    |\n   O    |\n        |\n        |\n        |\n-------------",
-       "   _____\n   |    |\n   O    |\n   |    |\n        |\n        |\n-------------",
-       "   _____\n   |    |\n   O    |\n  /|    |\n        |\n        |\n-------------",
-       "   _____\n   |    |\n   O    |\n  /|\\   |\n        |\n        |\n-------------",
-       "   _____\n   |    |\n   O    |\n  /|\\   |\n  /     |\n        |\n-------------",
-       "   _____\n   |    |\n   O    |\n  /|\   |\n  / \\   |\n        |\n-------------"]
 import random
+print("Hangman\nYou have 8 chanses before the man gets executed")
+sh={1:0,2:0}
+hanged=["\n\n\n\n\n\n-------------","        \n        |\n        |\n        |\n        |\n        |\n-------------","   _____\n        |\n        |\n        |\n        |\n        |\n-------------","   _____\n   |    |\n        |\n        |\n        |\n        |\n-------------","   _____\n   |    |\n   O    |\n        |\n        |\n        |\n-------------","   _____\n   |    |\n   O    |\n   |    |\n        |\n        |\n-------------","   _____\n   |    |\n   O    |\n  /|    |\n        |\n        |\n-------------","   _____\n   |    |\n   O    |\n  /|\\   |\n        |\n        |\n-------------","   _____\n   |    |\n   O    |\n  /|\\   |\n  /     |\n        |\n-------------","   _____\n   |    |\n   O    |\n  /|\\   |\n  / \\   |\n        |\n-------------"]
 word=random.choice(("hangman","hello","Why dont you choose the word?","cool","Turkmenistan"))
-win=0
-secret=[]
-guessed=[]
-used=[]
+sgu={1:[],2:[],3:[]}
 word=word.lower()
 for i in range(0,len(word)):
-    secret.append(word[i])
-    guessed.append("_")
-print("\nThe secret word:")
-print(" ".join(guessed))
-print("\n")
-while hangman<=8:
-    if "_" in guessed:
-        print()
-    else:
-        win=1
+    sgu[1].append(word[i])
+    sgu[2].append("_")
+print("\n\n\nThe secret word:"," ".join(sgu[2]),"\n")
+while sh[2]<=8:
+    if "_" not in sgu[2]:
         break
-    success=0
-    print(lista[hangman])
-    print("\n\nUsed letters:")
-    print(" ".join(used))
+    sh[1]=0
+    print(hanged[sh[2]])
+    print("\n\nUsed letters:\n"," ".join(sgu[3]))
     ans=input("\nGuess a letter")
-    used.append(ans)
-    for i in range(0, len(secret)):
-        if secret[i]==ans:
-            guessed[i]=ans
-            success=1
-    if success==0:
-        hangman+=1
-    print("\n\n\nThe secret word:")
-    print(" ".join(guessed))
-    print("\n")
-if win==1:
-    print(lista[hangman])
-    print("YOU WIN")
+    sgu[3].append(ans)
+    for i in range(0, len(sgu[1])):
+        if sgu[1][i]==ans:
+            sgu[2][i]=ans
+            sh[1]=1
+    if sh[1]==0:
+        sh[2]+=1
+    print("\n\n\nThe secret word:"," ".join(sgu[2]),"\n")
+if sh[2]==9:
+    print("%s \nYOU LOOSE"%(hanged[sh[2]]))
 else:
-    print(lista[hangman])
-    print("YOU LOOSE")
+    print("%s \nYOU WIN"%(hanged[sh[2]]))
+
+
 
