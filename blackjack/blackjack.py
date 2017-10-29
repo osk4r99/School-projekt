900509f09c229703bf5a5807732952f7d02be69b
osk4r99 <32760361+osk4r99@users.noreply.github.com>
2017-10-29 18:23:51 +0200

    Add files via upload

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,4 +1,4 @@
-#Blackjack version 1.0
+#Blackjack version 1.2
 from random import shuffle
 def betFunc():
     while True:
@@ -26,122 +26,120 @@ def answear():
         else:
             print("ERROR")
             continue
-def cardDraw(drawncards, drawncard, OneOrTwo):
+def cardDraw(drawncard, OneOrTwo, plyr, name):
+    global drawncards
     hand=[]
-    cardValue=[0, 0]
     for i in range(drawncards, drawncard):
         hand.append(cardDeck[drawncards])
+        cardsdranw[plyr].append(cardDeck[drawncards])
         drawncards+=1
+#    if len(hand)==2:
+#        hand[0]="Ace of Clubs"
+#        hand[1]="Queen of Clubs"
     for i in range(0, OneOrTwo):
         if "Ace" in hand[i]:
-            cardValue[i]+=11
+            cardValue[plyr].append(11)
         elif "King" in hand[i] or "Queen" in hand[i] or "Jack" in hand[i]:
-            cardValue[i]+=10
+             cardValue[plyr].append(10)
         else:
-            cardValue[i]+=int(hand[i][0:2]) #0:2 takes the 2 first letters from the string
+           cardValue[plyr].append(int(hand[i][0:2])) #0:2 takes the 2 first letters from the string
     if OneOrTwo==2:
-        cardTotal=int(cardValue[0])+int(cardValue[1])
-        print("You got a",hand[0],"and a",hand[1],"\nWich is a total of",cardTotal)
+        print("%s got a %s and a %s \nWich is a total of %s"%(name, hand[0], hand[1], sum(cardValue[plyr])))
     elif OneOrTwo==1:
-        cardTotal=int(cardValue[0])
-        print("You got a",hand[0])  
-    return cardTotal
+        print("%s got a %s"%(name, hand[0]))
 
 #Remove this when you make the full game
 money=100
 bet=0
 name=input("Enter NAME")
+tLos=0
+tWon=0
 
 
+while True:
+    while True:
+        lose=0
+        cardsdranw=[[], []]
+        cardValue=[[], []]
+        #0 is dealer 1 is player
+        
+        drawncards=0
+        cardDeck=[]
+        color=["Hearts", "Diamonds", "Spades", "Clubs"]
+        card=["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+        for i in range(0,13):
+            var="%s of %s"%(card[i], color[0])
+            var2="%s of %s"%(card[i], color[1])
+            var3="%s of %s"%(card[i], color[2])
+            var4="%s of %s"%(card[i], color[3])
+            cardDeck.extend((var, var2, var3, var4))
+        shuffle(cardDeck)
+#        print(cardDeck)
 
-a=0
-cardTotal=0
-while a<4:
-    drawncards=0
-    cardDeck=[]
-    color=["Hearts", "Diamonds", "Spades", "Clubs"]
-    card=["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-    for i in range(0,13):
-        var="%s of %s"%(card[i], color[0])
-        var2="%s of %s"%(card[i], color[1])
-        var3="%s of %s"%(card[i], color[2])
-        var4="%s of %s"%(card[i], color[3])
-        cardDeck.append(var)
-        cardDeck.append(var2)
-        cardDeck.append(var3)
-        cardDeck.append(var4)
-    shuffle(cardDeck)
-    print(cardDeck)
-
-    cardTotal=0
-    print("Total money",money,"€")
-#    print("Total los",tLos,"€")
-#    print("Total won",tWon,"€")
-    print("do you want to keep your bet?")
-    print("Your current bet is",bet,"€")
-    ans = answear()
-    if ans =="n":
+        print("Total money %s €"%(money))
+        print("Total los %s €"%(tLos))
+        print("Total won %s €"%(tWon))
         bet=betFunc()
-#    if money<=0:
-#        money+=sel()
-#        if money<=0:
-#            break
-    if bet>money:
-        bet=money
-    print("Hello",name,"\nwelcome to blackjack\n")
-    input("Press enter to start\n")
-    cardTotal+=cardDraw(drawncards, drawncards+2, 2)
-    drawncards+=2
-    print("Do you want another card\n")
-    ans = answear()
-    if ans == "n":
-        a=1
-    while a<1:
-        cardTotal+=cardDraw(drawncards, drawncards+1, 1)
-        drawncards+=1
-        print("Wich is a total of",cardTotal)
-        if cardTotal>=22:
-            a=3
-            cardTotal=0
-            print("\n\nDealer won\n")
-            money-=bet
-#            tLos+=bet
-        else:
+    #    if money<=0:
+    #        money+=sel()
+    #        if money<=0:
+    #            break
+        if bet>money:
+            bet=money
+        print("Hello %s\nwelcome to blackjack\n"%(name))
+        input("Press enter to start\n")
+        cardDraw(drawncards+2, 2, 1, name)
+        print()
+        cardDraw(drawncards+1, 1, 0, "Dealer")
+        if sum(cardValue[1])==21:
+            print("Player got a blackjack")
+            money+=bet*1.5
+            tWon+=bet*1.5
+            break
+        while True:
             print("Do you want another card\n")
             ans = answear()
             if ans == "n":
-                a=1
-    cardP1=cardTotal
-    cardTotal=0
-    while a<2:
-        input("Dealers turn\nPress enter\n")
-        cardTotal+=cardDraw(drawncards, drawncards+2, 2)
-        drawncards+=2
-        if cardTotal>=cardP1:
-            print("\n\nDealer won\n")
-            a=3
-            money-=bet
-#            tLos+=bet    
-        while a<3:
+                print("Dealers turn")
+                break
+            cardDraw(drawncards+1, 1, 1, name)
+            print("Wich is a total of",sum(cardValue[1]))
+            if sum(cardValue[1])>=22:
+                print("\n\nDealer won\n")
+                money-=bet
+                tLos+=bet
+                lose=1
+                break
+        while lose==0:
             input("\nPress enter to continiue\n")
-            cardTotal+=cardDraw(drawncards, drawncards+1, 1)
-            drawncards+=1
-            print("Wich is a total of",cardTotal,"\n")
-            if cardTotal>=22:
-                a=3
+            cardDraw(drawncards+1, 1, 0, "Dealer")
+            print("Wich is a total of",sum(cardValue[0]),"\n")
+            if sum(cardValue[0])>=17:
+                break
+        if lose==1:
+            pass
+        elif sum(cardValue[1])<sum(cardValue[0]) and  sum(cardValue[0])<22:
+            print("\n\nDealer won\n")
+            money-=bet
+            tLos+=bet
+        elif sum(cardValue[0])<sum(cardValue[1]):
                 print("\n\n%s won\n"%(name))
                 money+=bet
-#                tWon+=bet
-            elif cardP1<=cardTotal:
-                print("\n\nDealer won\n")
-                a=3
-                money-=bet
-#                tLos+=bet
+                tWon+=bet
+        elif sum(cardValue[0])==sum(cardValue[1]):
+            print("Its a draw")
+        elif sum(cardValue[0])>=22:
+            print("\n\n%s won\n"%(name))
+            money+=bet
+            tWon+=bet
+        break
+#        print(cardsdranw)
+#        print(cardValue)
+#        print(sum(cardValue[0]))
+#        print(sum(cardValue[1]))
     print("Do you want to play again\n")
     ans = answear()
     if ans == "n":
         print("Welcome back again",name)
         break
-    else:
-        a=0
-        print("\n\n","RESTART".center(70,"*"),"\n\n")
+    print("\n\n","RESTART".center(70,"*"),"\n\n")
