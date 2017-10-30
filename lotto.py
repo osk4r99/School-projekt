656134bebca8971597626884c226e165dc709ab3
osk4r99 <32760361+osk4r99@users.noreply.github.com>
2017-10-30 12:52:44 +0200

    Add files via upload

--- lotto.py
+++ lotto.py
@@ -1,41 +1,53 @@
-#lotto
-#lottorad=random.sample(lottoTal,7)
 import random
 lottoTal=[]
-
-minRad=[1, 12, 15, 25, 29, 30, 34]
-
+myRow=[]
+#myRow=[1, 12, 15, 25, 29, 30, 34]
+for i in range(0,7):
+    myRow.append(input("Enter number between 1-35 and dont use same number twice"))
+    while True:
+        try:
+            myRow[i]=int(myRow[i])
+            break
+        except:
+            myRow[i]=input("Enter number between 1-35 and dont use same number twice")
+    myRow.sort()
+    print(myRow)
 for i in range(0,35):
     lottoTal.append(i+1)
-lottoRad=random.sample(lottoTal,7)
+lottoRow=random.sample(lottoTal,7)
 success=0
-j=1
-sex=0
-fem=0
-fyra=0
-tre=0
+counter=1
+six=0
+five=0
+four=0
+three=0
+two=0
 while success<=6:
     success=0
-    lottoRad=random.sample(lottoTal,7)
-    lottoRad.sort()
-    for i in range(0,7):
-        if minRad[i]==lottoRad[i]:
-            success+=1
-    lottoRad.sort()
-    j+=1
+    lottoRow=random.sample(lottoTal,7)
+    lottoRow.sort()
+    for j in range(0,7):
+        for i in range(0,7):
+            if myRow[j]==lottoRow[i]:
+                success+=1
+    lottoRow.sort()
+    counter+=1
     if success==5:
-        sex+=1
+        six+=1
     elif success==4:
-        fem+=1
+        five+=1
     elif success==3:
-        fyra+=1
+        four+=1
     elif success==2:
-        tre+=1
-print(j)
-print(lottoRad)
+        three+=1
+    elif success==1:
+        two+=1
+print(counter)
+print(lottoRow)
 
 print()
-print("du hade",sex,"st 6 i rad")
-print("du hade",fem,"st 5 i rad")
-print("du hade",fyra,"st 4 i rad")
-print("du hade",tre,"st 3 i rad")
+print("You had %s 6 in a row"%(six))
+print("You had %s 5 in a row"%(five))
+print("You had %s 4 in a row"%(four))
+print("You had %s 3 in a row"%(three))
+print("You had %s 2 in a row"%(two))
